6ab87def2b419c1a0723d741127b0f537787e603
Karan Singh <iamkaransingh@gmail.com>
2015-11-26 21:06:42 -0500

    Vuforia enabled Unity app works on Android -- with Ariel

---
+++ UnityApp/StreamVRDroid/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	GameObject chan;
+
+	// Use this for initialization
+	void Start () {
+		chan = GameObject.Find ("unitychan").gameObject;
+		Debug.Log ("It begins!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			Debug.Log ("Woohoo!");
+		}
+	}
+}
