56800e0ea3f559340edc38731dc1fc39f59ebdae
Ariel Schvartzman <arielsc@mit.edu>
2015-11-30 00:44:21 -0500

    Begun process of porting code from Java to CS. There are still issues with Serialization on the server side -Ariel

--- src/AskServer.cs
+++ src/AskServer.cs
@@ -1,75 +1,229 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Text;
+using System.Collections.Generic;
 
-public class SynchronousSocketListener {
+public class AskServer : Thread {
 
     // Incoming data from the client.
+	Socket socket;
     public static string data = null;
+	double maxRadius = 1.0; 
+	public static Dictionary<int, Object> idMap;
+	public static int maxObjectId;
+	public static Interlocked engaged;
 
-    public static void StartListening() {
-        // Data buffer for incoming data.
-        byte[] bytes = new Byte[1024];
 
-        // Establish the local endpoint for the socket.
-        // Dns.GetHostName returns the name of the 
-        // host running the application.
-        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
-        IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+	//Class constructor 
+	public AskServer(Socket inputSocket){
+		Socket socket = inputSocket; 
+	}
 
-        // Create a TCP/IP socket.
-        Socket listener = new Socket(AddressFamily.InterNetwork,
-            SocketType.Stream, ProtocolType.Tcp );
+	Dictionary<Boolean, Integer> InsertObject(InsertQuery insertQuery){
+		double x = insertQuery.askObject.getX(); 
+		double y = insertQuery.askObject.getY(); 
+		double radius = insertQuery.askObject.getRadius(); 
+		double conservativeRadius = radius + maxRadius;
 
-        // Bind the socket to the local endpoint and 
-        // listen for incoming connections.
-        try {
-            listener.Bind(localEndPoint);
-            listener.Listen(10);
+		while(!engaged.CompareExchange(false, true)){
+			try {
+				Object[] neighbors = KDtree.RadialSearch(new double[]{x,y}, conservativeRadius, 100);
+				for(int i = 0; i < neighbors.length; i++){
+					AskObject neighbor = (AskObject) neighbors[i];
+					/** If there exists a point which might overlap (determined from globalRadius), reject. */
+					double distance = Math.Sqrt( Math.Pow(x-neighbor.getX(), 2) + Math.Pow(y-neighbor.getY(), 2) );
+					if(distance < neighbor.getRadius() + radius)
+						return new Dictionary<bool, int>(false, -1);
+				}
+				/** Otherwise, insert. */
+				insertQuery.askObject.objectId = maxObjectId;
+				KDtree.add(new double[]{x, y}, insertQuery.askObject);
+				idMap.Add(maxObjectId++, insertQuery.askObkject);
 
-            // Start listening for connections.
-            while (true) {
-                Console.WriteLine("Waiting for a connection...");
-                // Program is suspended while waiting for an incoming connection.
-                Socket handler = listener.Accept();
-                data = null;
+			}
+			catch (Exception e){
+				e.StackTrace();
+				return new Dictionary<bool, int>(false, -2);
+			}
+			/** Do not forget to hand over the lock. */
+			engaged.Exchange(false);
 
-                //An incoming connection needs to be processed.
-                // while (true) {
-                //     bytes = new byte[1024];
-                //     int bytesRec = handler.Receive(bytes);
+			return new Dictionary<bool, int>(true, insertQuery.askObject.objectId);
+		}
+	}
+	
+	Boolean DeleteObject(DeleteQuery deleteQuery){
+		/** Checks if the received queryId is in idMap. */
+		// TODO: 11/15/15  (Karan) It might be alright to say 'true'.
+		if(!idMap.ContainsKey(deleteQuery.queryId))
+			return false;
+		
+		AskObject askObject = idMap[deleteQuery.queryID];
+		try{
+			/** If the object is there, delete it. */
+			if (KDtree.FindValueAt(new double[]{askObject.getX(), askObject.getY()}) == false)
+				return false;
+			KDtree.RemoveAt(new double[]{askObject.getX(), askObject.getY()});
+			idMap.Remove(deleteQuery.queryId);
+		}
+		catch (Exception e){
+			e.StackTrace();
+			return false;
+		}
+		return true;
+	}
 
-                //     data = Encoding.ASCII.GetString(bytes,0,bytesRec);
-                //      Console.WriteLine( "Text received : {0}", data);
-                //     if (data.IndexOf("<EOF>") > -1) {
-                //         break;
-                //     }
-                // }
+	AskObject[] FetchObject(FetchQuery fetchQuery){
+		ArrayList<AskObject> askObjectList = new ArrayList();
+		try {
+			AskPredict askPredict = new AskPredict(fetchQuery);
+			Dictionary<double[], double[]> rectEstimates = askPredict.TotalPredict();
+			Object[] objects = KDtree.RadialSearch(rectEstimates.getKey(), rectEstimates.getValue(), 100);
+			foreach(Object listObj in objects) {
+				if(askPredict.Separator(new double[]{ ((AskObject)listObj).getX(), ((AskObject)listObj).getY() }))
+					askObjectList.add((AskObject)listObj);
+			}
+		}
+		catch (Exception e) {
+			e.StackTrace();
+		}
+		return askObjectList.toArray(new AskObject[askObjectList.size()]);
+	}
 
-                // Show the data on the console.
-                Console.WriteLine( "Text received : {0}", data);
-
-                // Echo the data back to the client.
-                byte[] msg = Encoding.ASCII.GetBytes("Blue");
-
-                handler.Send(msg);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-            }
-
-        } catch (Exception e) {
-            Console.WriteLine(e.ToString());
-        }
-
-        Console.WriteLine("\nPress ENTER to continue...");
-        Console.Read();
-
-    }
-
-    public static int Main(String[] args) {
-        StartListening();
-        return 0;
-    }
+	/** Handle is the primary function that processes and categorizes all client requests.
+     *
+     * @param object Expects an instance of an object inheriting Query.
+     * @return An object inheriting Result.
+     */
+	Object Handle(Object queryObject){
+		if(queryObject is TestQuery){
+			Console.WriteLine("Received a Test Query: "+((TestQuery) queryObject).test);
+			return new TestResult("Indeed!");
+		}
+		else if(queryObject is InsertQuery){
+			Console.WriteLine("Received an Insert Query.");
+			Dictionary<Boolean, Integer> result = InsertObject((InsertQuery) queryObject);
+			return new BoolIntResult(result.getKey(), result.getValue());
+		}
+		else if(queryObject is DeleteQuery){
+			Console.WriteLine("Received an Delete Query.");
+			return new BoolResult(DeleteObject((DeleteQuery) queryObject));
+		}
+		else if(queryObject is FetchQuery){
+			Console.WriteLine("Received an Fetch Query.");
+			return new ObjectResult(FetchObject((FetchQuery)queryObject));
+		}
+		else {
+			// TODO: 11/15/15   In case of a mismatch, I (Karan) recommend that this should throw an exception.
+			Console.WriteLine("The Query Object wasn't of the right kind.");
+			return new TestResult("Don't know what to do?");
+		}
+	}
+	
+//	/** This is the method that gets invoked on every thread's start().
+//     * Defers the processing to Handle(), and handles the networking components.
+//     */
+//	public void run(){
+//		try{
+//			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+//			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+//			Object inpObject = objectInputStream.readObject();
+//			objectOutputStream.writeObject(Handle(inpObject));
+//			objectInputStream.close();
+//			objectOutputStream.close();
+//			socket.close();
+//		}
+//		catch (Exception e){
+//			e.printStackTrace();
+//		}
+//	}
+	
+	public static void main(String [] args){
+		engaged = new Boolean(false);
+		kdTree = new KDtree(2);
+		idMap = new HashMap<int, Object>();
+		maxObjectId = 0;
+		try {
+			TcpListener serverSocket = new TcpListener(1234);
+			/** Waits to receive a client call. As soon as it gets one, forks a new instance to handle the same. */
+			while(true){
+				/** The accept() here is blocking. */
+				Socket socket = serverSocket.AcceptSocket();
+				AskServer askServer = new AskServer(socket);
+				/** Start the thread. */
+				askServer.Start();
+			}
+		}
+		catch (Exception e){
+			e.StackTrace();
+		}
+	}
 }
+
+//    public static void StartListening() {
+//        // Data buffer for incoming data.
+//        byte[] bytes = new Byte[1024];
+//
+//        // Establish the local endpoint for the socket.
+//        // Dns.GetHostName returns the name of the 
+//        // host running the application.
+//        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+//        IPAddress ipAddress = ipHostInfo.AddressList[0];
+//        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+//
+//        // Create a TCP/IP socket.
+//        Socket listener = new Socket(AddressFamily.InterNetwork,
+//            SocketType.Stream, ProtocolType.Tcp );
+//
+//        // Bind the socket to the local endpoint and 
+//        // listen for incoming connections.
+//        try {
+//            listener.Bind(localEndPoint);
+//            listener.Listen(10);
+//
+//            // Start listening for connections.
+//            while (true) {
+//                Console.WriteLine("Waiting for a connection...");
+//                // Program is suspended while waiting for an incoming connection.
+//                Socket handler = listener.Accept();
+//                data = null;
+//
+//                //An incoming connection needs to be processed.
+//                // while (true) {
+//                //     bytes = new byte[1024];
+//                //     int bytesRec = handler.Receive(bytes);
+//
+//                //     data = Encoding.ASCII.GetString(bytes,0,bytesRec);
+//                //      Console.WriteLine( "Text received : {0}", data);
+//                //     if (data.IndexOf("<EOF>") > -1) {
+//                //         break;
+//                //     }
+//                // }
+//
+//                // Show the data on the console.
+//                Console.WriteLine( "Text received : {0}", data);
+//
+//                // Echo the data back to the client.
+//                byte[] msg = Encoding.ASCII.GetBytes("Blue");
+//
+//                handler.Send(msg);
+//                handler.Shutdown(SocketShutdown.Both);
+//                handler.Close();
+//            }
+//
+//        } catch (Exception e) {
+//            Console.WriteLine(e.ToString());
+//        }
+//
+//        Console.WriteLine("\nPress ENTER to continue...");
+//        Console.Read();
+//
+//    }
+//
+//    public static int Main(String[] args) {
+//        StartListening();
+//        return 0;
+//    }
+//}
---
+++ src/Query.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Runtime.Serialization;
+/** Query is an abstract class (with support for serialization).
+ * All queries initiated from the client to the server must instantiate a class inheriting Query. */
+[Serializable]
+
+abstract class Query {
+	int queryId;
+}
+
+/** TestQuery serves the purpose of debugging. */
+class TestQuery:Query {
+	/** String test -- Test message to be transmitted to the server. */
+	String test;
+	
+	public TestQuery(String inputTest){
+		test = inputTest;
+	}
+}
+
+/** InsertQuery allows the specification of an instance of AskObject to be inserted in to the database. */
+class InsertQuery: Query {
+	/** AskObject askObject -- The object to be inserted. */
+	AskObject askObject;
+	
+	public InsertQuery(AskObject obj) {
+		askObject = obj;
+	}
+}
+
+/** InsertQuery contains objectId (a universal object identifier) to be deleted in to the database. */
+class DeleteQuery: Query {
+	/** int objectId -- The object to be deleted. */
+	int objectId;
+	
+	public DeleteQuery(int id) {
+		objectId = id;
+	}
+}
+
+/** In (near) future, FetchQuery will allow for specification of sensor data to pre-fetch objects. */
+class FetchQuery: Query {
+	double[] centerPoint = new double [2];
+	double[] speedVec = new double [3];
+	// speed is v_x, v_y, v_{theta}
+	double viewAngle;
+	double viewRadius;
+	double compassAngle;
+	
+	double RTT;
+	int[] objectIds;
+	
+	public FetchQuery(double[] _centerPoint, double[] _speedVec, double _viewAngle, double _viewRadius, double _compassAngle,  double _RTT, int[] _objectIds) {
+		centerPoint = _centerPoint;
+		speedVec = _speedVec;
+		viewAngle = _viewAngle;
+		viewRadius = _viewRadius;
+		compassAngle = _compassAngle;
+		
+		RTT = _RTT;
+		objectIds = _objectIds;
+	}
+}
+
