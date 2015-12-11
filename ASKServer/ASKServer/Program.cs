c7e4a418ce156c5d4fa5981026a9f2ab875881ed
Karan Singh <iamkaransingh@gmail.com>
2015-12-10 21:22:36 -0500

    Unit test for Client

--- ASKServer/ASKServer/Program.cs
+++ ASKServer/ASKServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using AskTest;
 using Newtonsoft.Json;
 
 namespace ASKServer
@@ -13,22 +14,28 @@ namespace ASKServer
 	class Program {
 
 		public static int Main(String[] args) {
-			Socket listener = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-			listener.Bind(new IPEndPoint(IPAddress.Any, 1234));
-			listener.Listen(100);
-
-			while (true) {
-				Socket handler = listener.Accept();
-				Console.Write ("Got a response!");
-				SomeObject so = new SomeObject ();
-				so.number = 21;
-				handler.Send(Encoding.ASCII.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(so)));
-//				byte[] instream = new byte[100000];
-//				handler.Receive (instream);
-//				Console.Write (Encoding.ASCII.GetString (instream));
-				handler.Shutdown(SocketShutdown.Both);
-				handler.Close();
-			}
+			Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect (new IPEndPoint(IPAddress.Parse("10.9.101.248"), 1234));
+			socket.Send (Encoding.ASCII.GetBytes (JsonConvert.SerializeObject (new Tuple<string, string>("InsertQuery", JsonConvert.SerializeObject(new  InsertQuery(new AskObject(new float[]{0,0}, "", 0, 0, 0)))))));
+			byte[] instream = new byte[100000];
+			int rec = socket.Receive (instream);
+			BoolIntResult so = Newtonsoft.Json.JsonConvert.DeserializeObject<BoolIntResult> (Encoding.ASCII.GetString(instream));
+			Console.WriteLine (so.integer);
+			socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect (new IPEndPoint(IPAddress.Parse("10.9.101.248"), 1234));
+			socket.Send (Encoding.ASCII.GetBytes (JsonConvert.SerializeObject (new Tuple<string, string>("InsertQuery", JsonConvert.SerializeObject(new  InsertQuery(new AskObject(new float[]{2,2}, "", 0, 0, 0)))))));
+			instream = new byte[100000];
+			socket.Receive (instream);
+			so = Newtonsoft.Json.JsonConvert.DeserializeObject<BoolIntResult> (Encoding.ASCII.GetString(instream));
+			Console.WriteLine (so.integer);
+			socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect (new IPEndPoint(IPAddress.Parse("10.9.101.248"), 1234));
+			socket.Send (Encoding.ASCII.GetBytes (JsonConvert.SerializeObject (new Tuple<string, string>("FetchQuery", JsonConvert.SerializeObject(new FetchQuery(new float[]{0,0}, new float[]{0,0}, 100, 100, new int[0]))))));
+			instream = new byte[100000];
+			socket.Receive (instream);
+			ObjectResult so2 = Newtonsoft.Json.JsonConvert.DeserializeObject<ObjectResult> (Encoding.ASCII.GetString(instream));
+			Console.WriteLine (so2.askObjects.Length);
+			return 0;
 		}
 	}
 }
