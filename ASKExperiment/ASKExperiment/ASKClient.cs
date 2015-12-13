663096b83a88a4afd766321128ce12a9e009ee97
sumegha <sumeghag@princeton.edu>
2015-12-13 14:00:57 -0500

    Experiment for our streaming model vs model without prediction

---
+++ ASKExperiment/ASKExperiment/ASKClient.cs
@@ -0,0 +1,150 @@
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading;
+
+
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ASKExperiment{
+
+
+
+
+
+//class ASKWorker {
+//
+//	public void FetchObjects (ConcurQueue<byte[]> queue) {
+//		BinaryFormatter bf = new BinaryFormatter ();
+//		MemoryStream ms = new MemoryStream ();
+//		byte[] instream = new byte[100000];
+//
+//		Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+//		socket.Connect (new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+//
+//		FetchQuery fq = new FetchQuery(new float[]{0, 0});
+//		bf.Serialize (ms, fq);
+//		socket.Send (ms.ToArray());
+//
+//		instream = new byte[100000];
+//		socket.Receive (instream);
+//		ms = new MemoryStream (instream);
+//		object obj2 = bf.Deserialize (ms);
+//
+//		if (obj2 is ObjectResult) {
+//			ObjectResult or = (ObjectResult) obj2;
+//			Debug.Log(or.askObjects.Length);
+//			foreach(AskObject askobject in or.askObjects){
+//				queue.Enqueue(askobject.objectstream);
+//			}
+//		} else {
+//			Debug.Log("Wrong kind of object.");
+//		}
+//	}
+//
+//	
+//	}
+
+//public class Client  {
+//	LocationService location = new LocationService ();	
+//	ASKWorker askWorker = new ASKWorker ();
+//	Thread clientThread;
+//	ConcurQueue<byte[]> queue;
+//
+//	void Start () {
+//		queue = new ConcurQueue<byte[]> ();
+//		clientThread = new Thread (() => askWorker.FetchObjects(queue));
+//		clientThread.Start ();
+//	}
+//
+//	void Update () {
+//		while (queue.Count > 0){
+//			byte[] newobj = queue.Dequeue();
+//			newobj.LoadObjectTree();
+//		}
+//		if (!clientThread.IsAlive) {
+//		}
+//	}
+//}
+public class Client{
+		Dictionary<int,AskObject> idMap;//Ask Karan how to update this in another thread;
+		public static void Start () {
+			BinaryFormatter bf = new BinaryFormatter ();
+			MemoryStream ms = new MemoryStream ();
+			int numberofobjects = 30;
+			//string[] str = new string[]{"ImageTarget", "ImageTarget (1)", "ImageTarget (2)"};
+			for(int i = 0; i<numberofobjects; i++){
+				Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.Connect (new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+				int length = 10000;
+				byte[] obj = new byte[length];
+				Random rnd = new Random ();
+				rnd.NextBytes (obj);
+
+				InsertQuery iq = new InsertQuery(new AskObject(new float[]{i*10,i*10},1,i,obj,0));
+
+				ms = new MemoryStream();
+				bf.Serialize(ms, iq);
+				socket.Send(ms.ToArray());
+				byte[] instream = new byte[100000];
+				socket.Receive (instream);
+				ms = new MemoryStream (instream);
+				object obj2 = bf.Deserialize (ms);
+
+				if (obj2 is BoolIntResult) {
+					BoolIntResult or = (BoolIntResult) obj2;
+					Console.WriteLine(or.integer);
+
+				} else {
+					Console.WriteLine("Wrong kind of object.");
+				}
+				socket.Close();
+			}
+		}
+public static void Main(String[] args) {
+	//StartClient();
+			Start();
+			Socket socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Connect (new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234));
+	//return 0;
+			double sum=0;
+			Stopwatch sw = new Stopwatch();
+			for (int i = 0; i < 30; i++) {
+				Thread.Sleep (400);
+				
+				FetchQuery fq = new FetchQuery(new float[]{i*10+0.1f, i*10});
+				fq.queryId = i;
+				BinaryFormatter bf = new BinaryFormatter ();
+				MemoryStream ms = new MemoryStream ();
+				bf.Serialize (ms, fq);
+				sw.Start();
+				socket.Send (ms.ToArray());
+
+				byte[] instream = new byte[100000];
+				socket.Receive (instream);
+				ms = new MemoryStream (instream);
+				object obj2 = bf.Deserialize (ms);
+
+				if (obj2 is ObjectResult) {
+					if (obj2 == null)
+						Console.WriteLine ("Null returned");
+					sw.Stop();
+					TimeSpan time= sw.Elapsed;
+					sum = sum + time.TotalSeconds;
+				}
+				
+			}
+			socket.Close();
+			double average = sum / 30;
+			Console.WriteLine ("Average fetching time={0}", average);
+		}
+	}
+}
+//Time left
---
+++ Basic/Basic/BasicQuery.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Basic {
+	[Serializable]
+	public class Query {
+
+		public int queryId;
+		int userId;
+		String deviceId;
+	}
+
+	[Serializable]
+	public class TestQuery:Query {
+
+		public String test;
+
+		public TestQuery(String inputTest){
+			test = inputTest;
+		}
+	}
+
+	[Serializable]
+	public class InsertQuery: Query {
+
+		public AskObject askObject;
+
+		public InsertQuery(AskObject obj) {
+			askObject = obj;
+		}
+	}
+
+	[Serializable]
+	public class DeleteQuery: Query {
+
+		public int objectId;
+
+		public DeleteQuery(int id) {
+			objectId = id;
+		}
+	}
+
+	[Serializable]
+	public class FetchQuery: Query {
+
+		public int targetId;
+		public float[] centerPoint;
+
+		public FetchQuery(float[] _centerPoint){
+			centerPoint = _centerPoint;
+		}
+	}
+}
+
